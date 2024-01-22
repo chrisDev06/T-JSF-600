c656f72f93478479bc2d46d5e46c97d40ecae9bd
Parisi <maxime.parisi@epitech.eu>
2024-01-22 17:14:55 +0100

    echange done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+const mongoose = require("mongoose")
+const Msg = require('./models/message');  
+const mongoDB = "mongodb+srv://maximeparisi:4nloXstxn8UHz1L7@cluster0.bbwhcld.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, {useNewUrlParser: true}).then(()=> {
+  console.log("connected")
+} )
 
 const express = require('express');
 const http = require('http');
@@ -6,25 +12,39 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {}
+});
 const PORT = process.env.PORT || 3001;
 
-const { join } = require('node:path');
-
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+const { join } = require('node:path'); 
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+  socket.on('helloServeur', (arg) => {        // connection client -> serveur
+    console.log(arg);
   });
 });
+io.on('connection', (socket) => {
+  socket.emit('helloClient', 'connection serveur -> client');  // co serveur-> client
+});
 
+
+io.on("connection", (socket)=> {
+  Msg.find().then(result => {
+    socket.emit("outputMessage", result)  
+  })
+  console.log("a user connected");
+  socket.emit("message", "hello")
+  socket.on("disconnect", ()=> {
+    console.log("user disconnected")
+  })
+  socket.on("chatmessage", msg => {
+    const message = new Msg({msg}) 
+    message.save().then(()=> {
+      io.emit("message", msg)
+    })
+  }) 
+})
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, '../frontend/public/index.html'));
