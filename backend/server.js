bbbcc485cf50534b37bf65f0162374eac3adad28
Parisi <maxime.parisi@epitech.eu>
2024-01-22 14:17:24 +0100

    input done

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,18 @@ const PORT = process.env.PORT || 3001;
 
 const { join } = require('node:path');
 
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 
