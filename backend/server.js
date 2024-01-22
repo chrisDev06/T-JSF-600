f7a876b6b5b63abb90370883293d6a036e96432c
Parisi <maxime.parisi@epitech.eu>
2024-01-22 12:29:07 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
-import { Server } from "socket.io";
 
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
+const io = new Server(server);
 const PORT = process.env.PORT || 3001;
+
+const { join } = require('node:path');
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '../frontend/public/index.html'));
+});
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
 
 
 
